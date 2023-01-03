bc81f1c6cdb098b6e58344746ab09c8e20940d1d
NoCastel <castelendy@gmail.com>
2023-01-03 21:55:26 +0200

    basic crud

--- src/main/java/classes/student/Student.java
+++ src/main/java/classes/student/Student.java
@@ -1,13 +1,9 @@
 package classes.student;
 
 import java.time.LocalDate;
+import java.time.Period;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table
@@ -19,16 +15,16 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student() {
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -64,11 +60,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
+        return Period.between(dob, LocalDate.now()).getYears();
     }
 
     @Override
--- src/main/java/classes/student/StudentConfig.java
+++ src/main/java/classes/student/StudentConfig.java
@@ -13,9 +13,9 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            Student megan = new Student("Megan", "megan@mail.com", LocalDate.of(1992, 12, 5), 23);
-            Student patrick = new Student("Patrick", "Patrick@mail.com", LocalDate.of(1992, 12, 5), 23);
-            Student alphonso = new Student("Alphonso", "Alphonso@mail.com", LocalDate.of(1992, 12, 5), 23);
+            Student megan = new Student("Megan", "megan@mail.com", LocalDate.of(1992, 12, 5));
+            Student patrick = new Student("Patrick", "patrick@mail.com", LocalDate.of(1996, 1, 5));
+            Student alphonso = new Student("Alphonso", "alphonso@mail.com", LocalDate.of(1998, 7, 5));
             repository.saveAll(List.of(megan, patrick, alphonso));
         };
     }
