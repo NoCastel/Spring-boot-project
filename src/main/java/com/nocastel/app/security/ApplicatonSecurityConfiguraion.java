d191d77fe351f85ce21930c02b469efc3b8bd4d7
NoCastel <castelendy@gmail.com>
2023-01-18 20:27:47 +0200

    in Memory user/pass

---
+++ src/main/java/com/nocastel/app/security/ApplicatonSecurityConfiguraion.java
@@ -0,0 +1,44 @@
+package com.nocastel.app.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicatonSecurityConfiguraion {
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicatonSecurityConfiguraion(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/index.html", "/css/*", "/js/*").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userJaneDoe = User.builder()
+                .username("janedoe")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(userJaneDoe);
+    }
+}
